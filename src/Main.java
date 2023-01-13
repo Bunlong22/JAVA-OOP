7f3f813f71d310d14548a99fb9794e847209c8fe
bunlong22 <suonbunlong93@gmail.com>
2023-01-13 09:53:26 +0700

    Lab1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("CADT ECOMMERCE GEN 8");
+        System.out.print("Hello world");
+        System.out.println("Hello world");
+//        println = space if write again, print= no space just go next each other.
     }
 
 }
